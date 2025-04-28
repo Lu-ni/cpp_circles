49572c03264c4971dc57749d4657e2250dcb11f5
Lucas Nicollier <lucas.nicollier@gmail.com>
2025-04-28 17:32:41 +0200

    01/ex02 done

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+    std::string var = "HI THIS IS BRAIN";
+    std::string* stringPTR = &var;
+    std::string& stringREF = var;
+
+    std::cout << &var << "\n";
+    std::cout << &stringPTR << "\n";
+    std::cout << &stringREF << "\n";
+
+    std::cout << var << "\n";
+    std::cout << *stringPTR << "\n";
+    std::cout << stringREF << "\n";
+}
