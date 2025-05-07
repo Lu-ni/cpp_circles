35d4d61479fb7941acaf10e864d012ad4874b043
Lucas Nicollier <lucas.nicollier@gmail.com>
2025-05-07 20:47:47 +0200

    Swap .replace by .erase/.insert

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -24,18 +24,18 @@ int main(int argc, char* argv[]) {
     std::string str1 = argv[2];
     std::string str2 = argv[3];
 
-    std::ifstream inputFile(filename);
-    if (!inputFile.is_open()) {
-        std::cerr << "Error: could not open input file " << filename << '\n';
-        return 1;
-    }
-
     if (str1.empty()) {
         std::cerr << "Error: str1 can't be empty\n";
         return 1;
     }
 
-    std::ofstream outputFile(filename + ".replace");
+    std::ifstream inputFile(filename.c_str());
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: could not open input file " << filename << '\n';
+        return 1;
+    }
+
+    std::ofstream outputFile((filename + ".replace").c_str());
     if (!outputFile.is_open()) {
         std::cerr << "Error: could not open output file " << filename + ".replace" << '\n';
         return 1;
@@ -45,11 +45,15 @@ int main(int argc, char* argv[]) {
     while (std::getline(inputFile, line)) {
         std::string::size_type pos = 0;
         while ((pos = line.find(str1, pos)) != std::string::npos) {
-            line.replace(pos, str1.length(), str2);
+            line.erase(pos, str1.length());
+            line.insert(pos, str2);
             pos += str2.length();
         }
         outputFile << line << '\n';
     }
+
+    inputFile.close();
+    outputFile.close();
 
     return 0;
 }
